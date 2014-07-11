c260ba514454adc06e6d016903c6eb3a200c4be1
Andrew Fleenor <andrewflnr@gmail.com>
2014-07-11 14:33:57 -0700

    rm cruft

--- foo.jl
+++ foo.jl
@@ -90,12 +90,6 @@ end
 data = Array(RGB, h*res, w*res)
 fill!(data, RGB(1,1,1))
 
-#for y in 1:h*res
-#    for x in 1:w*res
-#        data[y,x] = RGB(rand(),rand(),rand())
-#    end
-#end
-
 poly = [(100.0, 400.0), (500.0,200.0),(550.0, 600.0), (700.0,650.0), (700.0, 700.0), (150.0, 750.0)]
 poly2 = subsurf_loop(poly)
 poly3 = subsurf_loop(poly2)
