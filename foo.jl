4496cf8e8a9ffc4128488c14af90a6b6d9d92a2c
Andrew Fleenor <andrewflnr@gmail.com>
2014-07-17 17:19:11 -0700

    create CoordImage thingy
    abstracts conversion of float to pixel coords with
    a certain resolution

--- foo.jl
+++ foo.jl
@@ -7,23 +7,15 @@ h = 10
 # map "real" units to pixels
 res = 100 #px/cm
 
+img = makeImage(0, 0, 10, 10, 100)
 
-seed = Array((Float64, Float64), h, w)
-for y in 1:h
-    for x in 1:h
-        seed[y,x] = (rand(), rand())
-    end
-end
-data = Array(RGB, h*res, w*res)
-fill!(data, RGB(1,1,1))
-
-poly = Polygon([(100.0, 400.0), (500.0,200.0),(550.0, 600.0), (700.0,650.0), (700.0, 700.0), (150.0, 750.0)])
+poly = Polygon([(1.000, 4.000), (5.000,2.000),(5.500, 6.000), (7.000,6.500), (7.000, 7.000), (1.500, 7.500)])
 poly2 = subsurf(poly)
 poly3 = subsurf(poly2)
 poly4 = subsurf(poly3)
 
-draw(data, RGB(1,0,0), poly)
-draw(data, RGB(0,1,0), poly4)
+draw(img, RGB(1,0,0), poly)
+#draw(img, RGB(0,1,0), poly4)
 
 println("writing img")
-imwrite(data, "eckphbth.png")
+imwrite(img.data, "eckphbth.png")
--- util.jl
+++ util.jl
@@ -1,6 +1,8 @@
 using Images
 using Color
 
+r = x -> int(round(x))
+
 type Polygon
     points :: Array{(Float64, Float64),1}
 end
@@ -9,7 +11,40 @@ type CoordImage
     data :: Array{RGB, 2}
     top :: (Float64, Float64)
     dims :: (Float64, Float64)
-    res :: Int64
+    res :: Float64
+end
+
+function u2px(img::CoordImage, pt::(Float64, Float64))
+    x, y = pt
+    x, y = (x - img.top[1], y - img.top[2])
+    (r(x*img.res), r(y*img.res))
+end
+
+function px2u(img::CoordImage, pt::(Int64, Int64))
+    x, y = float(pt[1])/img.res, float(pt[2])/img.res
+    (x + img.top[1], y + img.top[2])
+end
+
+function plot(img::CoordImage, pt::(Int64, Int64), color)
+    ysize = size(img.data, 1)
+    xsize = size(img.data, 2)
+    if !((1 <= pt[1] <= xsize) && (1 <= pt[2] <= ysize))
+        # out of range
+        return
+    end
+    img.data[y,x] = color
+end
+
+function plot(img::CoordImage, pt::(Float64, Float64), color)
+    pt = u2px(pt)
+    plot(img, pt, color)
+end
+
+function makeImage(top, left, w, h, res)
+    pxw, pxh = r(w*res), r(h*res)
+    data = Array(RGB, pxh, pxw)
+    fill!(data, RGB(1,1,1))
+    Foo(data, (top, left), (w, h), res)
 end
 
 function each_edge(pairfn, p::Polygon)
@@ -20,31 +55,30 @@ function each_edge(pairfn, p::Polygon)
     end
 end
 
-function naive_line(img, color, x1, y1, x2, y2)
+function naive_line(img, color, x1::Int64, y1::Int64, x2::Int64, y2::Int64)
     xdiff = abs(x1 - x2)
     ydiff = abs(y1 - y2)
     start = (x1, y1)
     finish = (x2, y2)
     if xdiff == 0
         # line is perfectly vertical
-        starty = int(round(min(y1, y2)))
-        finishy = int(round(max(y1, y2)))
+        starty = min(y1, y2)
+        finishy = max(y1, y2)
         for y in starty:finishy
-            img[y, int(x1)] = color
+            plot(img, (x, y), color)
         end
     elseif xdiff >= ydiff
         # line is closer to horizontal
         if x2 < x1
             (start, finish) = (finish, start)
         end
-        starty = float(start[2])
-        finishy = float(finish[2])
-        slope = (finishy - starty)/float(xdiff)
-        for x in int(round(start[1])):int(round(finish[1]))
+        starty = start[2]
+        finishy = finish[2]
+        slope = (finishy - starty)/xdiff
+        for x in start[1]:finish[1]
             xdist = x - start[1]
-            y = round( start[2] + xdist*slope )
-            y = int(y)
-            img[y,x] = color
+            y = r( start[2] + xdist*slope )
+            plot(img, (x, y), color)
         end
     else
         # line is more vertical
@@ -52,10 +86,10 @@ function naive_line(img, color, x1, y1, x2, y2)
             (start, finish) = (finish, start)
         end
         slope = (finish[1] - start[1])/(finish[2] - start[2])
-        for y in int(round(start[2])):int(round(finish[2]))
+        for y in start[2]:finish[2]
             ydist = y - start[2]
-            x = round( start[1] + slope*ydist )
-            x = int(x)
+            x = r( start[1] + slope*ydist )
+            plot(img, (x, y), color)
             img[y,x] = color
         end
     end
