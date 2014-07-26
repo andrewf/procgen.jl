038f08d42bf0c3e6281789971f509db0feef2ef5
Andrew Fleenor <andrewflnr@gmail.com>
2014-07-25 23:32:06 -0700

    play with multiplying ridged noise

--- simplex.jl
+++ simplex.jl
@@ -7,14 +7,11 @@ function real_to_01(x)
     (atan(x) + pi/2)/pi
 end
 
-img = makeImage(0, 0, 5, 5, 80)
+img = makeImage(0, 0, 5, 5, 160)
 
 red = RGB(1,0,0)
 green = RGB(0,1,0)
 blue = RGB(0,0,1.0)
-
-minVal = 0
-maxVal = 0
 
 function rplus_to_01(x)
     1/(x+1)
@@ -25,17 +22,12 @@ function thisnoise(x, y)
 end
 
 each_pixel(img) do x,y
-    n = octaves(thisnoise, 4, 1.0, x, y)
-    #n = abs(n)
-    global minVal, maxVal
-    minVal = min(minVal, n)
-    maxVal = max(maxVal, n)
-    r = real_to_01(n)
+    n = abs(octaves((x,y)->perlin(get_gradient, x, y), 4, 2.0, x, y))
+    m = real_to_01(simplex(get_gradient, x/1, y/1))
+    r = m*(1-n)
+    #r = if m*(1-n) > .5 1 else 0 end
     RGB(r,r,r)
 end
 
 imwrite(img.data, "simplex.png")
-
-println("range of [", minVal, ", ", maxVal, "]")
-    
 
