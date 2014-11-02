2fcff4f7b3bba23bf3a20e2d69fb6b0215f41eee
Andrew Fleenor <andrewflnr@gmail.com>
2014-11-01 22:03:01 -0700

    mostly use vectors instead of tuples

--- util.jl
+++ util.jl
@@ -8,9 +8,8 @@ rplus_to_01(x) = 1 - exp(-x)
 # we scale atan to get derivative at 0 = 1
 real_to_01(x) = (atan(pi*x) + pi/2)/pi
 
-type Polygon
-    points :: Array{(Float64, Float64),1}
-end
+typealias Vec Vector{Float64}
+typealias Polygon Array{Float64, 2}
 
 function area(p::Polygon)
     # shoelace algorithm
@@ -31,18 +30,18 @@ type CoordImage
     res :: Float64
 end
 
-function u2px(img::CoordImage, pt::(Float64, Float64))
+function u2px(img::CoordImage, pt::Vec)
     x, y = pt
     x, y = (x - img.top[1], y - img.top[2])
-    (r(x*img.res), r(y*img.res))
+    [r(x*img.res); r(y*img.res)]
 end
 
-function px2u(img::CoordImage, pt::(Int64, Int64))
+function px2u(img::CoordImage, pt::Vector{Int64})
     x, y = float(pt[1])/img.res, float(pt[2])/img.res
-    (x + img.top[1], y + img.top[2])
+    [x + img.top[1]; y + img.top[2]]
 end
 
-function plot(img::CoordImage, pt::(Int64, Int64), color)
+function plot(img::CoordImage, pt::Vector{Int64}, color)
     ysize = size(img.data, 1)
     xsize = size(img.data, 2)
     if !((1 <= pt[1] <= xsize) && (1 <= pt[2] <= ysize))
@@ -52,23 +51,23 @@ function plot(img::CoordImage, pt::(Int64, Int64), color)
     img.data[pt[2],pt[1]] = color
 end
 
-function plot(img::CoordImage, pt::(Float64, Float64), color)
+function plot(img::CoordImage, pt::Vec, color)
     pt = u2px(img, pt)
     plot(img, pt, color)
 end
 
-function dot(img::CoordImage, pt::(Int64, Int64), color)
+function dot(img::CoordImage, pt::Vec, color)
     x, y = pt
     plot(img, pt, color)
-    plot(img, (x+1,y), color)
-    plot(img, (x,y+1), color)
-    plot(img, (x+1,y+1), color)
+    plot(img, [x+1,y], color)
+    plot(img, [x,y+1], color)
+    plot(img, [x+1,y+1], color)
 end
 
 function each_pixel(fn, img::CoordImage)
     for y in 1:size(img.data, 1)
         for x in 1:size(img.data, 2)
-            u = px2u(img, (x,y))
+            u = px2u(img, [x;y])
             img.data[y,x] = fn(u[1], u[2])
         end
     end
@@ -88,14 +87,20 @@ function render_by_pixels(fn, name, top, left, w, h, res)
 end
 
 function each_edge(pairfn, p::Polygon)
-    prev = p.points[length(p.points)] # start with last element
-    for pair in p.points
+    l = size(p)[2] # the width of the matrix
+    for i in range(1, l)
+        iprev = if i == 1 l else i - 1 end
+        pair = p[:,i]
+        prev = p[:,iprev]
         pairfn(prev, pair)
-        prev = pair
     end
 end
 
-function naive_line(img::CoordImage, color, x1::Int64, y1::Int64, x2::Int64, y2::Int64)
+function naive_line(img::CoordImage, color, p1::Vector{Int64}, p2::Vector{Int64})
+    x1 = p1[1]
+    y1 = p1[2]
+    x2 = p2[1]
+    y2 = p2[2]
     xdiff = abs(x1 - x2)
     ydiff = abs(y1 - y2)
     start = (x1, y1)
@@ -105,7 +110,7 @@ function naive_line(img::CoordImage, color, x1::Int64, y1::Int64, x2::Int64, y2:
         starty = min(y1, y2)
         finishy = max(y1, y2)
         for y in starty:finishy
-            plot(img, (x1, y), color)
+            plot(img, [x1, y], color)
         end
     elseif xdiff >= ydiff
         # line is closer to horizontal
@@ -118,7 +123,7 @@ function naive_line(img::CoordImage, color, x1::Int64, y1::Int64, x2::Int64, y2:
         for x in start[1]:finish[1]
             xdist = x - start[1]
             y = r( start[2] + xdist*slope )
-            plot(img, (x, y), color)
+            plot(img, [x, y], color)
         end
     else
         # line is more vertical
@@ -129,7 +134,7 @@ function naive_line(img::CoordImage, color, x1::Int64, y1::Int64, x2::Int64, y2:
         for y in start[2]:finish[2]
             ydist = y - start[2]
             x = r( start[1] + slope*ydist )
-            plot(img, (x, y), color)
+            plot(img, [x, y], color)
         end
     end
 end
@@ -138,24 +143,25 @@ function draw(img, color, poly::Polygon)
     each_edge(poly) do p1,p2
         p1 = u2px(img, p1)
         p2 = u2px(img, p2)
-        naive_line(img, color, p1[1],p1[2], p2[1],p2[2])
+        naive_line(img, color, p1, p2)
     end
 end
 
 # cut off each corner by for each edge, returning two points in the middle,
 # each some fraction of the total length from the end
 function subsurf(poly)
-    new_points = Array((Float64, Float64), 0)
-    each_edge(
-        (p1, p2) -> begin
-            d = (p2[1]-p1[1], p2[2]-p1[2])
-            r = 1.0/4.0
-            r2 = 1 - r
-            push!(new_points, (p1[1] + r*d[1], p1[2] + r*d[2]))
-            push!(new_points, (p1[1] + r2*d[1], p1[2] + r2*d[2]))
-        end,
-        poly
-    )
-    return Polygon(new_points)
+    l = size(poly)[2]
+    new_points :: Array{Float64, 2} = Array(Float64, 2, l*2)
+    i = 1
+    each_edge(poly) do p1, p2
+        d = p2 - p1
+        r = 1.0/4.0
+        r2 = 1 - r
+        # stick new points in the output array
+        new_points[:,2*i-1] = p1 + r*d
+        new_points[:,2*i] = p1 + r2*d
+        i+=1
+    end
+    return new_points
 end
 
