9df55137801c873893403957b0474fc23e7071c9
Andrew Fleenor <andrewflnr@gmail.com>
2014-11-04 09:42:10 -0800

    update old/polygon.jl for matrix polygon stuff

--- old/polygon.jl
+++ old/polygon.jl
@@ -9,14 +9,16 @@ res = 100 #px/cm
 
 img = makeImage(0, 0, 10, 10, 100)
 
-poly = Polygon([(1.000, 4.000), (5.000, 2.000), (5.500, 6.000),
-                (7.000, 6.500), (7.000, 7.000), (1.500, 7.500)])
+poly = transpose([1.0 4; 5 2; 5.5 6; 7 6.5; 7 7; 1.5 7.5])
+
+println("orig ", poly)
 poly2 = subsurf(poly)
 poly3 = subsurf(poly2)
 poly4 = subsurf(poly3)
 
 draw(img, RGB(1,0,0), poly)
-draw(img, RGB(0,1,0), poly4)
+draw(img, RGB(0,1,0), poly2)
+draw(img, RGB(0,0,1), poly4)
 
 println("writing img")
 imwrite(img.data, "eckphbth.png")
